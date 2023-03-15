e04fdea498927e4fdea0c73ec7d681ffb7e557b2
DESKTOP-4DBQPSO\14164 <1416477069@qq.com>
2023-03-15 21:09:32 +0800

    安装 log4js、moment、typeorm 配置 log4js

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerMiddleware } from './middleware/logger/logger.middleware';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(express.urlencoded({ extended: true }));
+  app.enableCors();
+  app.use(LoggerMiddleware);
+  const PORT = 3000;
+  await app.listen(PORT);
+  console.log(`
+  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  Booop Moment Server 服务已启动：http://localhost:${PORT}
+  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  `);
 }
+
 bootstrap();
